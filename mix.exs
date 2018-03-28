e57be99f18adedda31d78533f07048ba2814ed96
Anatoliy Kovalchuk <kr00lix@gmail.com>
2018-03-28 06:42:10 -0500

    fix coveralls

--- mix.exs
+++ mix.exs
@@ -13,8 +13,7 @@ defmodule GenWorker.MixProject do
       package: package(),      
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]    
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
