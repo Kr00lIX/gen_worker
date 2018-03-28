ea5b23f48783cb92508ebe379054993d75b3da46
Anatoliy Kovalchuk <kr00lix@gmail.com>
2018-03-28 06:27:28 -0500

    run mix format

--- lib/gen_worker/server.ex
+++ lib/gen_worker/server.ex
@@ -4,11 +4,12 @@ defmodule GenWorker.Server do
   require Logger
 
   defmodule State do
+    @moduledoc false
     defstruct [:run_at, :caller, :run_each, :last_called_at, :options]
   end
 
   def init(state) do
-    Logger.debug("Init worker with state: #{inspect(state)}")
+    Logger.debug("GenWorker: Init worker with state: #{inspect(state)}")
     schedule_work(state)
     {:ok, state}
   end
@@ -16,26 +17,31 @@ defmodule GenWorker.Server do
   def handle_info(:run_work, %{caller: caller, options: options} = state) do
     caller.run(options)
     schedule_work(state)
-    {:noreply, %{state | last_called_at: Timex.now()}}
+    {:noreply, %{state | last_called_at: time_now()}}
   end
 
-  defp schedule_work(%State{run_at: run_at, run_each: run_each, last_called_at: last_called_at}) do
-    current_time = Timex.now()
+  def time_now do
+    Timex.now()
+  end
 
-    call_after_msec =
-      current_time
-      |> Timex.set(run_at)
-      |> (fn call_at ->
-            if Timex.before?(current_time, call_at) &&
-                 (last_called_at == nil || Timex.before?(call_at, last_called_at)) do
-              call_at
-            else
-              Timex.shift(call_at, run_each)
-            end
-          end).()
-      |> Timex.diff(current_time, :milliseconds)
+  def delay_in_msec(%State{run_at: run_at, run_each: run_each, last_called_at: last_called_at}) do
+    current_time = time_now()
+    current_time
+    |> Timex.set(run_at)
+    |> (fn call_at ->
+          if Timex.before?(current_time, call_at) &&
+                (last_called_at == nil || Timex.before?(call_at, last_called_at)) do
+            call_at
+          else
+            Timex.shift(call_at, run_each)
+          end
+        end).()
+    |> Timex.diff(current_time, :milliseconds)
+  end
 
-    Logger.debug("Schedule run worker after #{call_after_msec} msec")
+  defp schedule_work(%State{}=state) do
+    call_after_msec = delay_in_msec(state)
+    Logger.debug("GenWorker run worker after #{call_after_msec} msec")
     Process.send_after(self(), :run_work, call_after_msec)
   end
 end
--- test/gen_worker_test.exs
+++ test/gen_worker_test.exs
@@ -1,26 +1,28 @@
 defmodule GenWorkerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest GenWorker
-
-  defmodule TestWorker do
-    use GenWorker, run_at: [microsecond: {1, 0}], run_each: [seconds: 1]
-    
-    def run(current_pid) do
-      send(current_pid, {:task_worker, Timex.now})
+  
+  describe "worker each second" do
+    defmodule TestWorker do
+      use GenWorker, run_at: [microsecond: {1, 0}], run_each: [seconds: 1]
+      
+      def run(current_pid) do
+        send(current_pid, {:task_worker, Timex.now})
+      end
     end
-  end
 
-  setup do
-    current_pid = self()
-    assert {:ok, pid} = TestWorker.start_link(current_pid)
-    [pid: pid]
-  end
+    setup do
+      current_pid = self()
+      assert {:ok, pid} = TestWorker.start_link(current_pid)
+      [pid: pid]
+    end
 
-  test "run worker each seconds" do
-    assert_receive {:task_worker, time1}, 1000
-    assert_receive {:task_worker, time2}, 1000
-    refute_receive {:task_worker, _time}
+    test "run worker each seconds" do
+      assert_receive {:task_worker, time1}, 1000
+      assert_receive {:task_worker, time2}, 1000
+      refute_receive {:task_worker, _time}
 
-    assert Timex.before?(time1, time2)
-  end
+      assert Timex.before?(time1, time2)
+    end
+  end 
 end
